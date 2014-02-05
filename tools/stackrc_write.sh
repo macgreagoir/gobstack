d596427a8638998689b3ac7c657c15a670bce555
Mick Gregg <mick.gregg@hp.com>
2014-02-05 14:55:23 +0000

    Be sure stackrc_write.sh has the env vars it expects.

--- tools/stackrc_write.sh
+++ tools/stackrc_write.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ## write out a stackrc file to source for openstack env vars
 
+source ${BASH_SOURCE%/*}/../defaults.sh
+
 grep export ${BASH_SOURCE%/*}/../defaults.sh > ~vagrant/stackrc
 sed -i "s/\${CONTROLLER_PUBLIC_IP}/${CONTROLLER_PUBLIC_IP}/" ~vagrant/stackrc
 sed -i "s/\${DEMO_TENANT_NAME}/${DEMO_TENANT_NAME}/" ~vagrant/stackrc
